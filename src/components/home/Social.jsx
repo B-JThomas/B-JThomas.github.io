821020682c37daa6f0be501924958c6869ee7581
B-JThomas <bailey.jthomas2@gmail.com>
2024-02-06 19:14:43 +1100

    Netlify

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -7,6 +7,7 @@ const Social = () => {
             href="https://github.com/B-JThomas" 
             className="home_social-icon" 
             target='_blank'
+            rel="noreferrer"
         >
             <i class="uil uil-github-alt"></i>
         </a>
@@ -15,6 +16,7 @@ const Social = () => {
             href="https://www.linkedin.com/in/bailey-thomas-394802248/" 
             className="home_social-icon" 
             target='_blank'
+            rel="noreferrer"
         >
             <i class="uil uil-linkedin"></i>
         </a>
@@ -23,6 +25,7 @@ const Social = () => {
             href="https://www.facebook.com/bailey.thomas.10297701" 
             className="home_social-icon" 
             target='_blank'
+            rel="noreferrer"
         >
             <i class="uil uil-facebook-f"></i>
         </a>
