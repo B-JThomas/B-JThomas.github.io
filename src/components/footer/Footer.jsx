a2284d0029124d4e0029f6a2d2511121698be0d2
B-JThomas <bailey.jthomas2@gmail.com>
2024-02-06 22:40:19 +1100

    Finish Contact, make website Netlify workable and add footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <section className="footer section" id="footer">
+        <p>hey</p>
+    </section>
+  )
+}
+
+export default Footer
