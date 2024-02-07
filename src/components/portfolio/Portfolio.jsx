d7fa8102edce2ba959b49f68987bc97cf4b1b27d
B-JThomas <bailey.jthomas2@gmail.com>
2024-02-07 18:01:43 +1100

    Cleaned up assets/footer and included Projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-import work1 from '../../assets/work1.jpg'
-import work2 from '../../assets/work2.jpg'
+import work1 from '../../assets/Project_img-1.png'
+import work5 from '../../assets/Project_img-2.png'
 import work3 from '../../assets/work3.jpg'
 import work4 from '../../assets/work4.jpg'
-import work5 from '../../assets/work5.jpg'
+import work2 from '../../assets/work5.jpg'
 import './portfolio.css';
 
 const Portfolio = () => {
@@ -36,11 +36,29 @@ const Portfolio = () => {
                 </ul>
             </div>
             <div className="portfolio_projects container grid">
-                <div className="portfolio_project active">
+                <div className= {workType % 2 === 0 ? "portfolio_project active" : "portfolio_project"}>
                     <img className="portfolio_project-img" src={work1} alt="Portfolio img" />
-                    <h3 className="portfolio_project-title" >Project Name(All)</h3>
+                    <h3 className="portfolio_project-title" >Smart Gadgets - Eccomerce</h3>
                     <div className="portfolio_project-data">
-                        <p>See More</p>
+                        <a 
+                        href='https://smartgadgets.com.au/' 
+                        target='_blank'
+                        rel="noreferrer" >
+                            See More
+                        </a>
+                        <i className="uil uil-arrow-right portfolio_project-icon"></i>
+                    </div>
+                </div>
+                <div className= {workType % 5 === 0 ? "portfolio_project active" : "portfolio_project"}>
+                    <img className="portfolio_project-img" src={work5} alt="Portfolio img" />
+                    <h3 className="portfolio_project-title" >Lemuse - Design Web Presence</h3>
+                    <div className="portfolio_project-data">
+                        <a 
+                        href='https://www.lemuse.com.au/' 
+                        target='_blank'
+                        rel="noreferrer" >
+                            See More
+                        </a>
                         <i className="uil uil-arrow-right portfolio_project-icon"></i>
                     </div>
                 </div>
@@ -52,25 +70,9 @@ const Portfolio = () => {
                         <i className="uil uil-arrow-right portfolio_project-icon"></i>
                     </div>
                 </div>
-                <div className= {workType % 2 === 0 ? "portfolio_project active" : "portfolio_project"}>
-                    <img className="portfolio_project-img" src={work3} alt="Portfolio img" />
-                    <h3 className="portfolio_project-title" >Project Name(Web)</h3>
-                    <div className="portfolio_project-data">
-                        <p>See More</p>
-                        <i className="uil uil-arrow-right portfolio_project-icon"></i>
-                    </div>
-                </div>
                 <div className= {workType % 3 === 0 ? "portfolio_project active" : "portfolio_project"}>
                     <img className="portfolio_project-img" src={work4} alt="Portfolio img" />
                     <h3 className="portfolio_project-title" >Project Name(App)</h3>
-                    <div className="portfolio_project-data">
-                        <p>See More</p>
-                        <i className="uil uil-arrow-right portfolio_project-icon"></i>
-                    </div>
-                </div>
-                <div className= {workType % 5 === 0 ? "portfolio_project active" : "portfolio_project"}>
-                    <img className="portfolio_project-img" src={work5} alt="Portfolio img" />
-                    <h3 className="portfolio_project-title" >Project Name(Design)</h3>
                     <div className="portfolio_project-data">
                         <p>See More</p>
                         <i className="uil uil-arrow-right portfolio_project-icon"></i>
